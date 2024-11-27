01d86d517b070f82027f02e509d97d4251a854e0
Roberto Guerrero <rjguerreroh@gmail.com>
2024-11-27 12:11:58 -0500

    add log port

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+
+  const PORT = process.env.PORT ?? 3000;
+  console.log(`App corriendo en puerto: ${PORT}`);
+  await app.listen(PORT);
 }
 bootstrap();
